8860dd9ba4bd96984d23b9433e5e21e2b68f1a99
John Reese <jpreese@mtu.edu>
2015-10-17 21:46:15 -0400

    Initial commit

---
+++ StringKata/StringCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace StringKata
+{
+    public class StringCalculator
+    {
+        public int Add(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return 0;
+            }
+
+            var delimeters = ",\n";
+
+            if (input.StartsWith("//"))
+            {
+                delimeters += input[2];
+                input = input.Substring(4, input.Length - 4);
+            }
+
+            var numbers = input.Split(delimeters.ToCharArray()).Select(int.Parse).ToList();
+            var negatives = numbers.Where(x => x < 0).ToList();
+
+            if (negatives.Any())
+            {
+                throw new ArgumentException(string.Format("Invalid numbers: {0}", string.Join(",", negatives)));
+            }
+
+            return numbers.Sum();
+        }
+    }
+}
---
+++ StringKataTests/StringKataTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using StringKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringKataTests
+{
+    [TestClass]
+    public class StringKataTests
+    {
+        [TestMethod]
+        public void EmptyStringReturnsZero()
+        {
+            "".ShouldBeEqualTo(0);
+        }
+
+        [TestMethod]
+        public void AddOneNumber()
+        {
+            "1".ShouldBeEqualTo(1);
+        }
+
+        [TestMethod]
+        public void AddTwoNumbers()
+        {
+            "1,2".ShouldBeEqualTo(3);
+        }
+
+        [TestMethod]
+        public void AddMultipleNumbers()
+        {
+            "1,2,3".ShouldBeEqualTo(6);
+        }
+
+        [TestMethod]
+        public void NewLineCanBeDelimeter()
+        {
+            "1\n2,3".ShouldBeEqualTo(6);
+        }
+
+        [TestMethod]
+        public void UserCanDefineOwnDelimeter()
+        {
+            "//;\n1;2".ShouldBeEqualTo(3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NegativeNumberThrowsException()
+        {
+            var sut = new StringCalculator();
+
+            sut.Add("-1,2");
+        }
+
+        [TestMethod]
+        public void NegativeNumberExceptionListsAllNegatives()
+        {
+            var sut = new StringCalculator();
+            var message = "";
+
+            try
+            {
+                sut.Add("-1,2,-3");
+            }
+            catch (ArgumentException e)
+            {
+                message = e.Message;
+            }
+
+            Assert.AreEqual("Invalid numbers: -1,-3", message);
+        }
+    }
+
+    public static class StringKataTestsHelper
+    {
+        public static void ShouldBeEqualTo(this string input, int expected)
+        {
+            var sut = new StringCalculator();
+            Assert.AreEqual(expected, sut.Add(input));
+        }
+    }
+}
